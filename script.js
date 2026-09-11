c452f81489ffd687b545e71936ef69b8bc5af093
shs2211097olvido-sys <shs2211097.olvido@csav.edu.ph>
2026-09-11 09:25:03 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 console.log("hello pring.");
+const Myname = "Nica Olvido";
+console.log("name: " + Myname);
+let age = 20;
+let number = "09705643839";
+console.log("Age: " + age)
+console.log("Number: " + number);
+
+
+const address = "Brgy 2, Victorias City";
+console.log("Address: " + address);
+
 
 
