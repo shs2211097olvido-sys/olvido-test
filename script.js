f7e101393d8dbb0889790c7f44e67d496af6b3a7
shs2211097olvido-sys <shs2211097.olvido@csav.edu.ph>
2026-09-11 12:23:03 +0800

    addEventListener

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ console.log(serviceHeading);
 const projectHeading = document.querySelector("#projects h2");
 console.log(projectHeading);
 
-//Text Context
+
 
 heading.textContent = "My portfolio";
 contactHeading.textContent = "Let's Connect!";
@@ -53,3 +53,27 @@ heading.style.backgroundColor = "yellow";
 contactHeading.style.color = "purple";
 serviceHeading.style.color = "purple";
 projectHeading.style.color = "purple";
+
+
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+
+})
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+
+    }
+});
